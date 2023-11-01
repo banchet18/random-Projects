2086d5ecdcb9386944201452a54f0edc2d6d81ed
banchet18 <banjarac6@gmail.com>
2023-11-01 14:18:33 +0530

    js overview

--- shopping-cart/main.js
+++ shopping-cart/main.js
@@ -34,16 +34,17 @@ let shopItemsData = [
 let generateShop = () => {
   return (shop.innerHTML = shopItemsData
     .map((x) => {
-      return `<div class="item">
-  <img width="220" src="images/img-1.jpg" alt="" />
+      let { id, name, price, desc, img } = x;
+      return `<div id=product-id-${id}  class="item" >
+  <img width="220" src=${img} alt="" />
   <div class="details">
-    <h3>Casual Shirt</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+    <h3>${name}</h3>
+    <p>${desc}</p>
     <div class="price-quantity">
-      <h2>$ 45</h2>
+      <h2>$ ${price}</h2>
       <div class="buttons">
         <i class="bi bi-dash-lg"></i>
-        <div class="quantity">0</div>
+        <div id=${id} class="quantity">0</div>
         <i class="bi bi-plus-lg"></i>
       </div>
     </div>

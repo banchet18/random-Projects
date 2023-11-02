867e269b61539fbf754523eb248030835e0b1946
banchet18 <banjarac6@gmail.com>
2023-11-02 11:35:45 +0530

    changes

--- shopping-cart/main.js
+++ shopping-cart/main.js
@@ -36,6 +36,9 @@ let generateShop = () => {
   return (shop.innerHTML = shopItemsData
     .map((x) => {
       let { id, name, price, desc, img } = x;
+
+      let search = basket.find((x) => x.id === id) || [];
+
       return `<div id=product-id-${id}  class="item" >
   <img width="220" src=${img} alt="" />
   <div class="details">
@@ -45,7 +48,9 @@ let generateShop = () => {
       <h2>$ ${price}</h2>
       <div class="buttons">
         <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-        <div id=${id} class="quantity">0</div>
+        <div id=${id} class="quantity">${
+        search.item === undefined ? 0 : search.item
+      }</div>
         <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
       </div>
     </div>
@@ -112,3 +117,5 @@ let calculation = () => {
       return x + y;
     }, 0);
 };
+
+calculation();

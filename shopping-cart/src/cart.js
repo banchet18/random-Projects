3271f04d771f708d3a3a411f1dba76fb08e15b2c
banchet18 <banjarac6@gmail.com>
2023-11-02 20:01:39 +0530

    2.21

---
+++ shopping-cart/src/cart.js
@@ -0,0 +1,41 @@
+let label = document.getElementById("label");
+let shoppingCart = document.getElementById("shopping-cart");
+
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
+calculation = () => {
+  let cartAmount = document.getElementById("cartAmount");
+  let mapReduce = basket
+    .map((x) => {
+      return x.item;
+    })
+    .reduce((x, y) => {
+      return x + y;
+    }, 0);
+
+  cartAmount.innerHTML = mapReduce;
+};
+
+calculation();
+
+let generateCard = () => {
+  if (basket.length !== 0) {
+    return (shoppingCart.innerHTML = basket
+      .map((x) => {
+        return `
+      <div className="cart-item">hello</div>
+      `;
+      })
+      .join(""));
+  } else {
+    shoppingCart.innerHTML = ``;
+    label.innerHTML = `
+    <h2>Cart is Empty</h2>
+    <a href="index.html">
+      <button class="HomeBtn">Back To Home</button>
+    </a>  
+    `;
+  }
+};
+
+generateCard();

68f0fc7238f22fbe99e9bd365c5819aaa54ebff6
banchet18 <banjarac6@gmail.com>
2023-11-03 15:52:30 +0530

    continue

--- shopping-cart/src/cart.js
+++ shopping-cart/src/cart.js
@@ -23,8 +23,31 @@ let generateCard = () => {
   if (basket.length !== 0) {
     return (shoppingCart.innerHTML = basket
       .map((x) => {
+        let { id, item } = x;
+
+        let search = shopItemsData.find((y) => id === y.id) || [];
+
         return `
-      <div className="cart-item">hello</div>
+      <div class="cart-item">
+        <img width = "100" src=${search.img} alt="" />
+        <div class="details">
+          <div class="title-price-x">
+            <h4 class = "title-price">
+              <p>${search.name}</p>
+              <p class="cart-item-price">$ ${search.price}</p>
+            </h4>
+            <i class="bi bi-x-lg"></i>
+          </div>
+
+          <div class="buttons">
+              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+              <div id=${id} class="quantity">${item}</div>
+              <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+          </div>
+
+          <h3></h3>
+        </div>
+      </div>
       `;
       })
       .join(""));
@@ -40,3 +63,62 @@ let generateCard = () => {
 };
 
 generateCard();
+
+let increment = (id) => {
+  let selectedId = id;
+  // console.log(basket);
+
+  // ! finding if selected id is equal to existing basket object id
+  let search = basket.find((x) => {
+    return selectedId.id === x.id;
+  });
+  // console.log(search);
+  // console.log(basket);
+
+  // ! checking after getting the search with find method is undefined or have something inside
+  // ! if nothing found then add the new object
+  // ! if found something then just add object item
+  if (search === undefined) {
+    basket.push({ id: selectedId.id, item: 1 });
+  } else {
+    search.item += 1;
+  }
+  // console.log(basket);
+  update(id);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let decrement = (id) => {
+  let selectedId = id;
+  // console.log(basket);
+  // ! finding if selected id is equal to existing basket object id
+  let search = basket.find((x) => {
+    return selectedId.id === x.id;
+  });
+  // console.log(search);
+  // console.log(basket);
+  // ! checking after getting the search with find method is undefined or have something inside
+  // ! if found something then just remove object item
+
+  if (search === undefined) return;
+  else if (search.item === 0) return;
+  else if (search) {
+    search.item -= 1;
+  }
+
+  update(id);
+  basket = basket.filter((x) => x.item !== 0);
+
+  generateCard();
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+let update = (id) => {
+  let selectedId = id;
+  let search = basket.find((x) => {
+    return selectedId.id === x.id;
+  });
+
+  let newId = document.getElementById(selectedId.id);
+  newId.innerHTML = search.item;
+  calculation();
+};

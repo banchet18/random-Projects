2081d0e2f12c91baba1034447dce4ee595008b19
banchet18 <banjarac6@gmail.com>
2023-11-04 14:43:41 +0530

    changes

--- practice-shopping-cart/src/app.js
+++ practice-shopping-cart/src/app.js
@@ -1,4 +1,24 @@
 let mainsection = document.getElementById("main-section");
 
-let generateLandingCart = () => {};
+let generateLandingCart = () => {
+  mainsection.innerHTML = shopItemsData.map((x) => {
+    console.log(x);
+    let { id, name, price, desc, img } = x;
+    return `<div id="${id}" class="one-cart">
+  <img src=${x.img} alt="" />
+  <div class="name-desc">
+    <h3>${name}</h3>
+    <p>${desc}</p>
+  </div>
+  <div class="price-quantity">
+    <h3>$ ${price}</h3>
+    <div class="quantity">
+      <button class="minus-plus">-</button>
+      <div id="${id}">0</div>
+      <button class="minus-plus">+</button>
+    </div>
+  </div>
+</div>`;
+  });
+};
 generateLandingCart();
